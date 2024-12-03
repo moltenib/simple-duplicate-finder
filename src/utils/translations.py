3fc060bfa2076ab2be245602bd7c12c51b25f959
Bruno <brunomolteni@outlook.com>
2024-12-03 10:24:49 -0300

    Default to English on Windows (temporary)

--- src/utils/translations.py
+++ src/utils/translations.py
@@ -6,27 +6,27 @@ def set_up_translations():
     # Choose the system locale, if available
     system_locale = locale.get_locale()
 
-    if system_locale is None:
+    if os.name == 'nt':
         language = 'en'
 
     else:
         language = locale.getlocale()[0][:2]
+    
+        locale_path = os.path.join(
+                os.path.dirname(__file__),
+                '../../resources/locales')
 
-    locale_path = os.path.join(
-            os.path.dirname(__file__),
-            '../../resources/locales')
+        gettext.bindtextdomain('messages', locale_path)
+        gettext.textdomain('messages')
 
-    gettext.bindtextdomain('messages', locale_path)
-    gettext.textdomain('messages')
+        try:
+            translation = gettext.translation(
+                    'messages', localedir=locale_path, languages=[language])
 
-    try:
-        translation = gettext.translation(
-                'messages', localedir=locale_path, languages=[language])
+            translation.install()
 
-        translation.install()
-
-    except FileNotFoundError:
-        gettext.install('messages', localedir=locale_path)
-        print("Defaulting to English translations")
+        except FileNotFoundError:
+            gettext.install('messages', localedir=locale_path)
+            print("Defaulting to English translations")
 
 
