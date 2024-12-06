ffad3aa7849ce844850c654f7417cef8c7c40aaf
Bruno <stamby@github.com>
2024-12-06 08:38:26 +0100

    Remove unused class

--- src/utils/dict_to_class.py
+++
@@ -1,5 +0,0 @@
-class DictToClass:
-    def __init__(self, dict_):
-        for key, value in dict_.items():
-            attr_name = key.replace('-', '_')
-            setattr(self, attr_name, value)
